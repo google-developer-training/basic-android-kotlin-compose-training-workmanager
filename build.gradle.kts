dc1d52e06012209d494a531a4451fe21136393ac
John Shea <sheajohn@google.com>
2023-09-28 15:49:55 -0400

    Implement edge-to-edge

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,13 +18,13 @@
 
 buildscript {
     extra.apply {
-        set("compose_compiler_version", "1.4.7")
-        set("lifecycle_version", "2.6.1")
+        set("compose_compiler_version", "1.5.3")
+        set("lifecycle_version", "2.6.2")
     }
 }
 
 plugins {
-    id("com.android.application") version "8.0.2" apply false
-    id("com.android.library") version "8.0.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
+    id("com.android.application") version "8.1.1" apply false
+    id("com.android.library") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
