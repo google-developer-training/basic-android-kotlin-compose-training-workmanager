52bd6b95d2fde0c643f5011cc67019a218fbf28b
John Shea <sheajohn@google.com>
2023-10-11 14:54:04 -0400

    Update versions to the latest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,9 +67,9 @@ android {
 
 dependencies {
 
-    implementation(platform("androidx.compose:compose-bom:2023.09.00"))
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation(platform("androidx.compose:compose-bom:2023.10.00"))
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-ktx:1.8.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.runtime:runtime")
