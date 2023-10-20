6005f4417b15c542a7436c4ed1d2fe03e0d6aa35
John Shea <sheajohn@google.com>
2023-10-20 10:47:23 -0400

    Refactor to use edge-to-edge API

--- app/src/main/java/com/example/bluromatic/BlurActivity.kt
+++ app/src/main/java/com/example/bluromatic/BlurActivity.kt
@@ -22,6 +22,7 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
@@ -30,6 +31,7 @@ import com.example.bluromatic.ui.theme.BluromaticTheme
 
 class BlurActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             BluromaticTheme {
